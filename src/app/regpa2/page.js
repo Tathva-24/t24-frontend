c84fc7d6dc0afadc25e94894766ce227a6ddd251
Richie James <richiejames955@gmail.com>
2024-10-05 18:17:06 +0530

    commiting

---
+++ src/app/regpa2/page.js
@@ -0,0 +1,17 @@
+"use client";
+import React, { useState } from "react";
+import styles from "./reg.module.css";
+import Image from "next/image";
+
+function Fullpage() {
+    return (
+      <>
+        <div className={styles.page}>
+          hi hello
+        </div>
+      </>
+    );
+  }
+  
+  export default Fullpage;
+  
